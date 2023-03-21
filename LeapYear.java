840825bfab67da05118f0f397e431443666cd1cf
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-21 11:30:20 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,15 @@
+class LeapYear
+{
+public static void main(String[] args)
+{
+int a=2000;
+if(a%4==0)
+{
+System.out.println(a +"is a leap year");
+}
+else
+{
+System.out.println(a +"is npt a leap year");
+}
+}
+}
