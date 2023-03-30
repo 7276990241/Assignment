089be83e66a2c2d45ab64828ddf503ec33d71b9a
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-30 10:41:38 +0530

    Add files via upload

---
+++ AreaCalculate.java
@@ -0,0 +1,10 @@
+package cognizant.shapes;
+
+public class AreaCalculate {
+	public static void main(String[] args)
+	{
+		Rectangle rectangle = new Rectangle(5,10);
+		rectangle.calculateArea();
+	}
+
+}
