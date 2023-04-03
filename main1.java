0bc0ddc70c30b22150a3ea755937838d20e4a81d
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-04-03 13:54:45 +0530

    Add files via upload

---
+++ main1.java
@@ -0,0 +1,36 @@
+package inheritance1;
+
+class A
+{
+	void print()
+	{
+		System.out.println("The class A");
+	}
+}
+class B extends A
+{
+	void show()
+	{
+		System.out.println("The class B");
+
+	}
+}
+class C extends B
+{
+	void display()
+	{
+		System.out.println("The class C");
+	}
+}
+
+
+public class main1
+{
+	public static void main(String[] args)
+	{
+		C c=new C();
+		c.print();
+		c.show();
+		c.display();
+	}
+}
