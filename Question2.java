f9c6a598e32397f53804f116f0ca420ae90c5ed9
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-04-06 14:27:32 +0530

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,38 @@
+package assignments;
+
+class Sports
+{
+	String getName()
+	{
+		return "Generic Sports";
+	}
+	void getNumberofTeamMembers()
+	{
+		System.out.println("Each team has n players in" +getName());
+	}
+}
+class Football extends Sports
+{
+	//Override
+	String getName()
+	{
+		return "Football class";
+	}
+	//Override
+	void getNumberofTeamMembers()
+	{
+		System.out.println("Each team has 11 players in" +getName());
+	}
+}
+public class Question2
+{
+	public static void main(String[] args)
+	{
+		Sports c1 = new Sports();
+		Football c2 = new Football();
+		System.out.println(c1.getName());
+		c1.getNumberofTeamMembers();
+		System.out.println(c2.getName());
+		c2.getNumberofTeamMembers();
+	}
+}
